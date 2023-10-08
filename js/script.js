79080738d119743e99e5d4f838849e8ac619713c
Paplavskaya <any-lion@mail.ru>
2023-10-08 18:26:22 +0300

    feat - added task_2 in script.js

--- js/script.js
+++ js/script.js
@@ -5,3 +5,14 @@ let getResult = (number1, number2, number3) => (number1 - number2) / number3;
 console.log(getResult(25, 10, 3));
 
 
+/*Сделайте функцию, которая возвращает куб числа и его квадрат. Число 
+передается параметром.*/
+
+let getResult1 = function(number){
+    let cubeNumber = number ** 3;
+    let squareNumber = number ** 2;
+    return `${cubeNumber}, ${squareNumber}`;
+}
+console.log(getResult1(5));
+
+
