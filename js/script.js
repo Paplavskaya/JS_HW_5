d670989d8690f8492a9d94d91dea9454fe7a13cf
Paplavskaya <any-lion@mail.ru>
2023-10-08 18:16:24 +0300

    feat - added task_1 in script.js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*Сделайте функцию, которая отнимает от первого числа второе и делит на 
+третье. Числа передаются параметром.*/
+
+let getResult = (number1, number2, number3) => (number1 - number2) / number3;
+console.log(getResult(25, 10, 3));
+
+
